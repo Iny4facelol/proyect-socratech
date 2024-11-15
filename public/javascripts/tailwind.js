af99f195a2213dbdb8f748956da3014f07057575
Iny4facelol <150298787+Iny4facelol@users.noreply.github.com>
2024-11-15 20:34:51 +0100

    third

---
+++ public/javascripts/tailwind.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        mainBlue: " #11888f",
+        mainOrange: "#e3b741",
+        secondBlue: "#BDD6DB",
+      },
+    },
+  },
+};
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,6 +4,8 @@ const indexController = require("../controllers/indexController");
 
 router.get("/", indexController.openIndex);
 
-
+router.get("/test", (req, res) => {
+  res.render("test");
+});
 
 module.exports = router;
