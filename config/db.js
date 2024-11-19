bfc05aa5effdd224ca8674d96ad154a950dccf17
Iny4facelol <150298787+Iny4facelol@users.noreply.github.com>
2024-11-19 10:38:43 +0100

    fifth

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
-//Configurar la conexión con la base de datos.
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "pawfound"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
-  if(err) {
+  if (err) {
     console.log(err);
     return;
   }
